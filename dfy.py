cbacc8909b9bd40e269b9630238916ec05729058
Numerologic <bigtowncreations@gmail.com>
2023-03-08 05:18:31 +0100

    Update dfy.py
    Complete revision and changes for an output of the script in the terminal, not in a GUI

--- dfy.py
+++ dfy.py
@@ -1,157 +1,72 @@
+import os
 from pytube import YouTube, Playlist
-import PySimpleGUI as gui
 
-#
-# draw the layout of the window
-#
-frame_layout_1 = [[gui.In(font='NewsGothicMT 16'),gui.Push()]]
-frame_layout_2 = [[gui.Radio('a playlist', 'RADIO1', default=True,font='NewsGothicMT 16'),gui.Radio('a single song', 'RADIO1', default=False, key="-SONG-",font='NewsGothicMT 16'),gui.Push()]]
-frame_layout_3 = [
-    [
-        gui.In(font='NewsGothicMT 16', key='-PLAYLIST-'),
-        
-        #Set inital_folder if you want the browse folder button start the search in a specific folder
-        gui.FolderBrowse(button_text='Browse',key='-BROWSE-',font='NewsGothicMT 14',size=9, initial_folder=''), 
-        
-        gui.Push()]
-]
-frame_layout_4 = [
-    [gui.Push(),gui.Output(size=(70, 10),font='NewsGothicMT 12'),gui.Push()],
-    [gui.Push(),
-        gui.B('Download',font='NewsGothicMT 14',size=9),
-        gui.B('Cancel',font='NewsGothicMT 14',size=9),
-        gui.B('Close',font='NewsGothicMT 14',size=9),
-        gui.Push()]]
+# display colors and text transformations
+CBOLD = '\033[1m'
+CEND = '\033[0m'
 
-#
-# main layout
-#
-layout = [    
-    [gui.Frame('Paste a url from Youtube', frame_layout_1,relief='flat', font='NewsGothicMT 16')],
-    [gui.Frame('What are you downloading?', frame_layout_2,relief='flat', font='NewsGothicMT 16')],
-    [gui.Frame('Select a destination folder', frame_layout_3,relief='flat', font='NewsGothicMT 16')],
-    [gui.Frame('', frame_layout_4,relief='ridge', font='NewsGothicMT 16')],
-]
+os.system('clear')
 
-#
-# start drawing the window
-#
-window = gui.Window('Download an audio playlist/song/podcast from YouTube', layout)
+# asking informations inputs
+ans = input("Paste the url from YouTube:")
+plst = 'playlist?'
 
-
-while True:
-
-    event, values = window.read()  
-
-    #listen for specials events
-    #(by example, push a particular button)
-    #
-    if event in (gui.WIN_CLOSED, 'Cancel') :
-        break   
-
-    elif event in (gui.WIN_CLOSED, 'Close') :
-        window.close()
-
-    elif values['-SONG-'] == False: #Is it a playlist or single song/podcast/audio file?       
-        
-        #
-        #start evaluating the download of PLAYLIST
-        #
+# choose between playlist or single album
+isPlst = ans.find(plst)
+if (isPlst > 0):
+    # download playlist
+    try:
+        oplaylist = Playlist(ans)
+        vl = oplaylist.trimmed(oplaylist.playlist_url)
+        c = 0
+        r = str()        
+        parent_dir = '/Users/arnaud/Music'
+        artist = input("> Artist/author name?")
+        playlist_name = input("> Playlist/album name?")
+        destination_folder = os.path.join(parent_dir, artist+'/'+playlist_name)
         try:
-            oplaylist = Playlist(values[0])
-        except:
-            print('Reading Error!')
-            window.refresh() 
-            
-        else:
-            print('\r','*'*3,'Playlist','*'*3)
-            
-            print('1. Number of titles:',oplaylist.length)
-            print('2. Name of the playlist:', oplaylist.title)        
-            print('3. URL of the playlist:', oplaylist.playlist_url)
-            print('4. Directory of the playlist:', values['-PLAYLIST-'])     
-
-            try:
-                vl = oplaylist.trimmed(oplaylist.playlist_url)
-                c = 0
-                r = str()
-            except:
-                print("Extraction error!")
-                window.refresh()
-            else:
-                print('5. Content of the playlist:')
-                window.refresh()#display the message in real time
-                
-                
-                for i in vl:
-                    c = c + 1
-                    i_yt = YouTube(i)
-
-                    for s in i_yt.streams.filter(only_audio=True):
-                        try:
-                            #
-                            #choose the highest quality for AUDIO FILE (video excluded!)
-                            #
-                            stream_to_dwnld = i_yt.streams.get_by_itag(140)
-                            stream_to_dwnld.download(output_path=values['-PLAYLIST-'])
-                                                            
-                        except:                            
-                            print(f'MP4 version is not available.\rTrying to download a downgrade one...')
-                            window.refresh()                          
-                            
-                            try:
-                                stream_to_dwnld = i_yt.streams.get_by_itag(251)
-                                stream_to_dwnld.download(output_path=values['-PLAYLIST-'])
-
-                            except:
-                                print(f'WEBM (160Kbps) version is not available.\rTrying to download a downgrade one...')
-                                window.refresh()                               
-
-                                try:
-                                    stream_to_dwnld = i_yt.streams.get_by_itag(250)
-                                    stream_to_dwnld.download(output_path=values['-PLAYLIST-'])
-
-                                except:
-                                    print(f'WEBM (70Kbps) version is not available.\rTrying to download a downgrade one...')
-                                    window.refresh() 
-
-                                    try:
-                                        stream_to_dwnld = i_yt.streams.get_by_itag(249)
-                                        stream_to_dwnld.download(output_path=values['-PLAYLIST-'])
-
-                                    except:
-                                        print(f'WEBM (50Kbps) version is not available.\rTrying to download a downgrade one...')
-                                        window.refresh()                                    
-
-                    
-                    print(f"{i_yt.title} downloaded with success!")
-                    window.refresh()
-                
-                window.close()
-    else:
-        #
-        #start evaluating the download of SINGLE AUDIO FILE
-        #
+            # donwload of playlist start!
+            os.makedirs(destination_folder)
+            os.system('clear')
+            teaser = f"\r*** Playlist of {oplaylist.length} songs ***\nName: {CBOLD}{oplaylist.title}{CEND}"
+            d = f"\rDestination folder {CBOLD}{destination_folder}{CEND} created!\r"
+            print(teaser + d)
+            for i in vl:
+                c = c + 1
+                i_yt = YouTube(i)
+                for s in i_yt.streams.filter(only_audio=True):
+                    try:
+                        stream_to_dwnld = i_yt.streams.get_by_itag(140)
+                        stream_to_dwnld.download(output_path=destination_folder)
+                    except Exception as e:
+                        raise e
+                s = f"{c}. {CBOLD}{i_yt.title}{CEND}...\x1b[6;30;42m downloaded successfully! \x1b[0m"
+                print(s)
+        except Exception as e:
+            raise e 
+    except Exception as e:
+        raise e
+else:
+    # download single album/file
+    try:
+        osingle = YouTube(ans)
+        parent_dir = '/Users/arnaud/Music'
+        artist = input("> Artist/author name?")
+        playlist_name = input("> Playlist/album name?")
+        destination_folder = os.path.join(parent_dir, artist+'/'+playlist_name)
+        print('\r','*'*3,'One song from',osingle.title, '*'*3)
         try:
-            osingle = YouTube(values[0])
-        except:
-            print('Exception raised when reading the single file.')
-            window.refresh()
-        else:
-            print('\r','+'*3,'Single song','+'*3)            
-            print('Title:',osingle.title)       
-            print('Directory of the file:', values['-PLAYLIST-'])    
-            window.refresh()
-            try:
-                print('\nDownload the file...')    
-                window.refresh()
-                stream_to_dwnld = osingle.streams.get_by_itag(140)
-                stream_to_dwnld.download(output_path=values['-PLAYLIST-'])
-            except:
-                print(f'MP4 version is not available.')
-                window.refresh()
-            else:
-                print(f"Downloaded with success!")
-                window.refresh()
+            os.makedirs(destination_folder)
+            os.system('clear')
+            d = f"Destination folder {CBOLD}{destination_folder}{CEND} created!\r"
+            print(d)
+            stream_to_dwnld = osingle.streams.get_by_itag(140)
+            stream_to_dwnld.download(output_path=destination_folder)
+            s = f"{CBOLD}{stream_to_dwnld.title}{CEND}...\x1b[6;30;42m downloaded successfully! \x1b[0m"
+            print(s)
+        except Exception as e:
+            raise e
+    except Exception as e:
+        raise e
 
-        
+# end if
